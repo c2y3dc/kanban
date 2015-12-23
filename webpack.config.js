a2812db039e514ea59218e97da720c95b6e09488
Ervin Chow <ervinchow@gmail.com>
2015-12-23 18:07:37 +0800

    adds webpack config set up

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+var merge = require('webpack-merge');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+var common = {
+  entry: PATHS.app,
+  // Given webpack-dev-server runs in-memory, we can drop
+  // `output`. We'll look into it again once we get to the
+  // build chapter.
+  /*output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },*/
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      }
+    ]
+  },
+  plugins: [
+    // Important! move HotModuleReplacementPlugin below
+    //new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    })
+  ]
+};
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
