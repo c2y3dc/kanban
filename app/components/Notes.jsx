b90dad4bc34f1bdb447eca73fd50b0716b4c87cd
Ervin Chow <ervinchow@gmail.com>
2016-04-17 16:23:41 -0700

    adds code refactor to Notes component

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import Note from './Note.jsx';
+import React from 'react'
+import Note from './Note.jsx'
 
-// export default ({notes}) => {
 export default ({notes, onEdit, onDelete}) => {
-    return (
-        <ul className="notes">{notes.map(note =>
-            <li className="note" key={note.id}>
-                <Note
-                    task={note.task}
-                    onEdit={onEdit.bind(null, note.id)}
-                    onDelete={onDelete.bind(null, note.id)} />
-            </li>
-        )}</ul>
-    );
+  return (
+    <ul className='notes'>{notes.map((note) =>
+      <li className='note' key={note.id}>
+        <Note
+          task={note.task}
+          onEdit={onEdit.bind(null, note.id)}
+          onDelete={onDelete.bind(null, note.id)} />
+      </li>
+    )}</ul>
+  )
 }
