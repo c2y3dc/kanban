8b476de556d5ad7fb4882405eb5680be98728a99
Ervin Chow <ervinchow@gmail.com>
2016-04-19 16:22:43 -0700

    adds persistance logic to app

--- app/index.jsx
+++ app/index.jsx
@@ -4,4 +4,10 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './components/App.jsx';
 
+import alt from './libs/alt'
+import storage from './libs/storage'
+import persist from './libs/persist'
+
+persist(alt, storage, 'app')
+
 render(<App />, document.getElementById('app'));
