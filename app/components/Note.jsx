bd913b25c78fd57edd9a90298eb080c7d3b2a559
Ervin Chow <ervinchow@gmail.com>
2016-04-24 21:50:05 -0700

    adds droptarget decorator set up

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import {DragSource} from 'react-dnd'
+import {DragSource, DropTarget} from 'react-dnd'
 import ItemTypes from '../constants/itemTypes'
 
 const noteSource = {
@@ -10,15 +10,27 @@ const noteSource = {
 	}
 }
 
+const noteTarget ={
+	hover(targetProps, monitor) {
+		const sourceProps = monitor.getItem()
+
+		console.log("dragging note", sourceProps, targetProps)
+	}
+}
+
 @DragSource(ItemTypes.NOTE, noteSource, (connect) =>({
 	connectDragSource: connect.dragSource()
 }))
 
+@DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+  connectDropTarget: connect.dropTarget()
+}))
+
 export default class Note extends Component {
 	render() {
-		const {connectDragSource, id, onMove, ...props} = this.props
-		return connectDragSource(
+		const {connectDragSource, connectDropTarget, id, onMove, ...props} = this.props
+		return connectDragSource( connectDropTarget(
 			<li {...props}>{props.children}</li>
-		);
+		))
 	}
 }
