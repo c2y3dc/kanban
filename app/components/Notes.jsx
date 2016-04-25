96549cfbffb3934cb699cb36952a73a0826d1a5b
Ervin Chow <ervinchow@gmail.com>
2016-04-24 19:55:56 -0700

    adds dragsource and notes sorting set up

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Editable from './Editable'
+import Note from './Note'
 
 export default ({notes, onValueClick, onEdit, onDelete}) => {
   return (
     <ul className='notes'>{notes.map((note) =>
-      <li className='note' key={note.id}>
+      <Note className='note' key={note.id} id={note.id}>
         <Editable
           editing ={note.editing}
           value={note.task}
           onValueClick={onValueClick.bind(null, note.id)}
           onEdit={onEdit.bind(null, note.id)}
           onDelete={onDelete.bind(null, note.id)} />
-      </li>
+      </Note>
     )}</ul>
   )
 }
