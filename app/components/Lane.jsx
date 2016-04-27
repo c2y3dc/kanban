ba64db1b9771f4c2964998ccf14ffc4cb63dc9c7
Ervin Chow <ervinchow@gmail.com>
2016-04-27 15:21:26 -0700

    adds logic to display log messages only in desired state ie empty lane

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -10,11 +10,12 @@ import ItemTypes from '../constants/itemTypes'
 
 const noteTarget = {
 	hover( targetProps, monitor ){
-		const targetId = targetProps.lane.id
 		const sourceProps = monitor.getItem()
 		const sourceId = sourceProps.id
-		
-		console.log(`source: ${sourceId}, target: ${targetId}`)
+
+		if(!targetProps.lane.notes.length){
+			console.log(`source: ${sourceId}, target: ${targetProps}`)
+		}
 	}
 }
 
